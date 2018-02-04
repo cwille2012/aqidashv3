bbc003d681546a9e8a1c9e14322ba9915416b6cf
Chris Wille <chris@christopherwille.com>
2018-02-03 21:13:23 -0500

    app update

--- app.js
+++ app.js
@@ -2312,8 +2312,96 @@ socket.on('open', function() {
                     });
                 }
 
+                _onHover({ x, y, object }) {
+                    this.setState({ x, y, hoveredObject: object });
+                }
+
+                _onMouseMove(evt) {
+                    if (evt.nativeEvent) {
+                        this.setState({ mousePosition: [evt.nativeEvent.offsetX, evt.nativeEvent.offsetY] });
+                    }
+                }
+
+                _onMouseEnter() {
+                    this.setState({ mouseEntered: true });
+                }
+
+                _onMouseLeave() {
+                    this.setState({ mouseEntered: false });
+                }
+
+                _renderTooltip() {
+                    const { x, y, hoveredObject } = this.state;
+
+                    if (!hoveredObject) {
+                        return null;
+                    }
+
+                    var tooltipExists = !!document.getElementById('tooltip');
+
+                    var hoveredObjectHTML = hoveredObject.address;
+
+                    if (tooltipExists) {
+                        document.getElementById('tooltip').style.position = "absolute";
+                        document.getElementById('tooltip').style.zIndex = 99999;
+                        document.getElementById('tooltip').style.color = '#fff';
+                        document.getElementById('tooltip').style.background = 'rgba(0, 0, 0, 0.8)';
+                        document.getElementById('tooltip').style.padding = '4px';
+                        document.getElementById('tooltip').style.fontSize = '10px';
+                        document.getElementById('tooltip').style.maxWidth = '300px';
+                        document.getElementById('tooltip').style.left = x + 'px';
+                        document.getElementById('tooltip').style.top = y + 'px';
+                        document.getElementById('tooltip').style.cursor = 'pointer';
+                        document.getElementById('tooltip').setAttribute('text-decoration', 'none!important');
+                        //console.log(hoveredObject);
+                        //codeLatLng(hoveredObject.centroid[1], hoveredObject.centroid[0]);
+                    }
+
+                    var locationPlace;
+                    var timeSpent = String(Math.round((((hoveredObject.points.length * 2) / 60) * 100) / 100)) + " hours";
+
+                    if (hoveredObject.points.length == 252) {
+                        locationPlace = "Georgetown University Student Center";
+                    } else if (hoveredObject.points.length == 37) {
+                        locationPlace = "Georgetown University Library";
+                    } else if (hoveredObject.points.length == 108) {
+                        locationPlace = "Sheraton Pentagon City";
+                    } else if (hoveredObject.points.length == 48) {
+                        locationPlace = "Air Force Memorial";
+                    } else if (hoveredObject.points.length == 50) {
+                        locationPlace = "Dama Cafe";
+                    } else if (hoveredObject.points.length == 30) {
+                        locationPlace = "US Court of federal Claims";
+                    } else if (hoveredObject.points.length == 47) {
+                        locationPlace = "Hyatt Rosyln Suites";
+                    } else if (hoveredObject.points.length == 28) {
+                        locationPlace = "Miriam's Kitchen";
+                    } else if (hoveredObject.points.length == 25) {
+                        locationPlace = "Federal Reserve Building";
+                    } else if (hoveredObject.points.length > 15) {
+                        locationPlace = "Unlisted Location";
+                    } else {
+                        locationPlace = "In transit";
+                    }
+
+                    return ( < div id = "tooltip"
+                        style = {
+                            { left: x, top: y }
+                        } >
+                        <
+                        div > { 'Index: ' + hoveredObject.index } < /div> <
+                        div > { 'Longitude: ' + hoveredObject.centroid[0] } < /div> <
+                        div > { 'Latitude: ' + hoveredObject.centroid[1] } < /div> <
+                        div > { 'Location: ' + locationPlace } < /div> <
+                        div > { 'Time spent: ' + timeSpent } < /div> <
+                        div > { 'Points: ' + hoveredObject.points.length } < /div> <
+                        div > { 'Test value: ' + hoveredObject } < /div>  < /
+                        div >
+                    );
+                }
+
                 render() {
-                    const { viewport, data } = this.state;
+                    const { viewport, data, iconMapping, mousePosition, mouseEntered } = this.state;
 
                     return ( <
                         MapGL {...viewport }
@@ -2323,6 +2411,9 @@ socket.on('open', function() {
                         <
                         DeckGLOverlay viewport = { viewport }
                         data = { data || [] }
+                        mousePosition = { mousePosition }
+                        mouseEntered = { mouseEntered }
+                        onHover = { this._onHover.bind(this) }
                         /> < /
                         MapGL >
                     );
@@ -2330,14 +2421,6 @@ socket.on('open', function() {
             }
 
             render( < Root / > , document.getElementById('mapHolder').appendChild(document.createElement('div')));
-
-
-
-
-
-
-
-
 
 
 

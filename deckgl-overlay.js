5a070b4ff0b2ff1fd2636e62d1f2c9cd550b3bde
Chris Wille <chris@christopherwille.com>
2018-02-04 10:20:49 -0500

    overlay fix

--- deckgl-overlay.js
+++ deckgl-overlay.js
@@ -34,8 +34,8 @@ export default class DeckGLOverlay extends Component {
 
     static get defaultViewport() {
         return {
-            longitude: -77.0369,
-            latitude: 38.9072,
+            longitude: -0.1276250,
+            latitude: 51.5033640,
             zoom: 13,
             minZoom: 9,
             maxZoom: 18,
